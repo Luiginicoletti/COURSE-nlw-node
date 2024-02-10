88027e391618ea6737822df724f77060e8702b01
luiginicoletti <callmenicolett@gmail.com>
2024-02-10 10:39:18 -0300

    create routes

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../../lib/prisma";
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post("/polls/pollId/votes", async (request, reply) => {
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const { pollId } = voteOnPollParams.parse(request.params);
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
+
+    return reply.status(201).send();
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,28 +1,15 @@
 import fastify from "fastify";
 import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-app.post("/polls", async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-
-  console.log(request.body);
-
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return reply.status(201).send({ pollId: poll.id });
-});
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll(app));
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP Server Running!");
