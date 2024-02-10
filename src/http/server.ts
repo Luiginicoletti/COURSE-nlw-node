bb8d5049fc49f089f86ca1082522eea5696b225a
luiginicoletti <callmenicolett@gmail.com>
2024-02-10 15:41:48 -0300

    poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,12 +4,18 @@ import { PrismaClient } from "@prisma/client";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: "ni-code-v",
+  hook: "onRequest",
+});
+
 app.register(createPoll);
 app.register(getPoll);
-app.register(voteOnPoll(app));
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP Server Running!");
